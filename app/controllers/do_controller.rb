5c0d58d83ce035eec3636761992a687f3c73c5f7
Nikita Tselishchev <brideofchanges@gmail.com>
2016-03-26 07:18:57 +0000

    Added answers loop for question

--- app/controllers/do_controller.rb
+++ app/controllers/do_controller.rb
@@ -2,5 +2,6 @@ class DoController < ApplicationController
     def show
         @list1 = Test.where(id: params[:test_id])
         @list2 = Question.where(test_id: @list1.all)
+        @list3 = Answer.where(question_id: @list2.all)
     end
 end
